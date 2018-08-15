eedfdfe05f079f81dcbbe1bd35fa8647a42640e9
Revathi Shanmugam <srevathikarthik@gmail.com>
2018-08-15 13:59:13 -0700

    Restart/reset code

--- js/app.js
+++ js/app.js
@@ -62,3 +62,9 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+
+$('.restart').on('click', function() {
+    location.reload();
+});
